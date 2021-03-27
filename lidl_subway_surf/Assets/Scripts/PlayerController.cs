122cf817214edc7158975666a555036361a1a6e3
your name <mtaz.samuel@gmail.com>
2021-03-27 16:29:21 +0100

    nový script co v něm asi je??

--- lidl_subway_surf/Assets/Scripts/PlayerController.cs
+++ lidl_subway_surf/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    float score = 0;
-    int coin_count = 0;
-
     [Header("running")]
     public float speed;
     public float speed_multiplier_over_time;
@@ -18,6 +16,8 @@ public class PlayerController : MonoBehaviour
     public float lane_offset;
     public int lane_count; //na každou stranu
     int current_lane;
+    [HideInInspector]
+    public Vector3 move_vector;
 
     [Header("jumping")]
     public float jump_force;
@@ -33,20 +33,15 @@ public class PlayerController : MonoBehaviour
     bool sliding;
     public float slide_jump_down_force; //kvalitní jméno
 
-    [Header("audio")]
-    public AudioSource jump;
-    public AudioSource slide;
-    public AudioSource die;
-    public AudioSource move;
-
     [Header("dependencies")]
-    public Animator animator;
-    public Text score_text;
+    public UnityEvent e_change_lane;
+    public UnityEvent e_jump;
+    public UnityEvent e_slide;
+    public UnityEvent e_die;
+    public UnityEvent e_pick_up_coin;
+
     CharacterController cc;
     SwipeManager swipe_input;
-    public GameObject pause_ui;
-    public GameObject play_ui;
-    public GameObject die_ui;
 
     void Start()
     {
@@ -54,11 +49,6 @@ public class PlayerController : MonoBehaviour
         swipe_input = GetComponent<SwipeManager>();
         current_lane = 0;
         y_velocity = 0;
-        play_ui.SetActive(true);
-        pause_ui.SetActive(false);
-        die_ui.SetActive(false);
-        score = 0;
-        Time.timeScale = 1;
     }
 
     void Update()
@@ -73,36 +63,30 @@ public class PlayerController : MonoBehaviour
         y_velocity -= gravity * Time.deltaTime;
         if (grounded && y_velocity <= 0) y_velocity = -5f;
 
-        Vector3 move_vector = Vector3.forward * speed * Time.deltaTime; //dopredu
+        move_vector = Vector3.forward * speed * Time.deltaTime; //dopredu
         move_vector += Vector3.up * y_velocity * Time.deltaTime; // nahoru
         move_vector += Vector3.left * (transform.position.x - current_lane * lane_offset) * switch_lane_speed * Time.deltaTime; // do strany
         cc.Move(move_vector);
-
-        animator.SetFloat("x_speed", move_vector.x * 20);
-        score += Mathf.Round(transform.position.z / 2) - score;//¯\_(ツ)_/¯
-        score_text.text = $"score: {score + coin_count*20}";
     }
 
     void Inputs()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow) || swipe_input.SwipeRight) { current_lane += 1; move.Play(); }
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || swipe_input.SwipeLeft) { current_lane -= 1; move.Play(); }
+        if (Input.GetKeyDown(KeyCode.RightArrow) || swipe_input.SwipeRight) { current_lane += 1; e_change_lane.Invoke(); }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || swipe_input.SwipeLeft) { current_lane -= 1; e_change_lane.Invoke(); }
         if ((Input.GetKeyDown(KeyCode.UpArrow) || swipe_input.SwipeUp) && grounded)
         {
-            animator.SetTrigger("jump");
             y_velocity = jump_force;
             real_slide_time = 0;
-            jump.Play();
+            e_jump.Invoke();
         }
         if (Input.GetKeyDown(KeyCode.DownArrow) || swipe_input.SwipeDown)
         {
-            animator.SetTrigger("slide");
             y_velocity = -1 * slide_jump_down_force;
             cc.height = collider_size;
             cc.center = new Vector3(0, collider_size / 2, 0);
             real_slide_time = slide_time;
             sliding = true;
-            slide.Play();
+            e_slide.Invoke();
         }
 
         if (Input.GetKeyDown(KeyCode.R)) Die();
@@ -122,9 +106,8 @@ public class PlayerController : MonoBehaviour
     {
         if (other.transform.CompareTag("Coin"))
         {
-            coin_count += 1;
-            other.gameObject.GetComponent<AudioSource>().Play();
-            Destroy(other.gameObject, 0.1f);
+            e_pick_up_coin.Invoke();
+            other.gameObject.SetActive(false);
         }
     }
 
@@ -134,33 +117,8 @@ public class PlayerController : MonoBehaviour
         if (hit.normal.y < 0.4f) Die();
     }
 
-    #region tlacitka
-    public void Pause()
-    {
-        Time.timeScale = 0;
-        play_ui.SetActive(false);
-        pause_ui.SetActive(true);
-    }
-
-    public void UnPause()
-    {
-        Time.timeScale = 1;
-        play_ui.SetActive(true);
-        pause_ui.SetActive(false);
-    }
-
-    public void Reset()
-    {
-        SceneManager.LoadScene(0);
-    }
-    #endregion
-
     void Die()
     {
-        Time.timeScale = 0;
-        die_ui.SetActive(true);
-        play_ui.SetActive(false);
-        pause_ui.SetActive(false);
-        die.Play();
+        e_die.Invoke();
     }
 }
---
+++ lidl_subway_surf/Assets/Scripts/SceneController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour
+{
+    float score = 0;
+    int coin_count = 0;
+
+    [Header("audio")]
+    public AudioSource jump;
+    public AudioSource slide;
+    public AudioSource die;
+    public AudioSource move;
+    public AudioSource coin;
+
+    [Header("dependencies")]
+    public Animator animator;
+    public Text score_text;
+    public GameObject pause_ui;
+    public GameObject play_ui;
+    public GameObject die_ui;
+    PlayerController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<PlayerController>();
+        play_ui.SetActive(true);
+        pause_ui.SetActive(false);
+        die_ui.SetActive(false);
+
+        Time.timeScale = 1;
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("x_speed", controller.move_vector.x * 20);
+
+        score += Mathf.Round(transform.position.z / 2) - score;//¯\_(ツ)_/¯
+        score_text.text = $"score: {score + coin_count * 20}";
+    }
+
+    public void Jump()
+    {
+        jump.Play();
+        animator.SetTrigger("jump");
+    }
+    public void Slide()
+    {
+        slide.Play();
+        animator.SetTrigger("slide");
+    }
+    public void Move()
+    {
+        move.Play();
+    }
+    public void Die()
+    {
+        die.Play();
+        Time.timeScale = 0;
+        die_ui.SetActive(true);
+        play_ui.SetActive(false);
+        pause_ui.SetActive(false);
+    }
+
+    public void Pick_up_coin()
+    {
+        coin_count += 1;
+        coin.Play();
+    }
+
+
+    #region tlacitka
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        play_ui.SetActive(false);
+        pause_ui.SetActive(true);
+    }
+
+    public void UnPause()
+    {
+        Time.timeScale = 1;
+        play_ui.SetActive(true);
+        pause_ui.SetActive(false);
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(0);
+    }
+    #endregion
+
+}
